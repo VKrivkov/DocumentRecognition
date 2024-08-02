e1e908442d4b7df01d39ad373263ee905fa0ccfe
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-08-02 14:42:03 +0200

    Initial Commit

---
+++ Rectangles/RectanglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectanglesApp.swift
+//  Rectangles
+//
+//  Created by Vitaly Krivkov on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RectanglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
